fff6446d8df38b4c07856f55a2c3cde430ca615c
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-01 16:42:28 +0100

    +

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 const restaurantSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -8,16 +9,36 @@ const restaurantSchema = new mongoose.Schema({
   email: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   menu: { type: Array },
-  photos: { type: Array, of: String }, 
+  photos: { type: Array, of: String },
   openingHours: {
     type: Object,
     required: true,
     properties: {
       days: { type: Array, required: true, enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] },
-      openTime: { type: String, required: true }, 
+      openTime: { type: String, required: true },
       closeTime: { type: String, required: true },
     }
   },
 });
 
-module.exports = mongoose.model('Restaurant', restaurantSchema);
+const restaurantOwnerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  restaurant: { type: restaurantSchema, required: true }
+});
+
+restaurantOwnerSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcryptjs.compare(candidatePassword, this.password);
+};
+
+restaurantOwnerSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('RestaurantOwner', restaurantOwnerSchema);
