995d5119b02c5eccce4ed53c74f0f1c19316dd68
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-02 06:05:59 +0100

    +

--- routes/avisRoutes.js
+++ routes/avisRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const middlewareAuth = require('../middleware/authMiddleware');
 const { creerAvis, obtenirAvisRestaurant } = require('../controllers/avisController');
 
-// Utilisez la fonction creerAvis comme callback pour POST /avis
 router.post('/', middlewareAuth, creerAvis);
-
-// Utilisez la fonction obtenirAvisRestaurant comme callback pour GET /restaurants/:restaurantId/avis
 router.get('/restaurants/:restaurantId/avis', obtenirAvisRestaurant);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,20 @@
 const { Router } = require('express');
-const { registerUser, loginUser, userProfile, updateProfile , allRestaurants , allMenus} = require('../controllers/userController');
+const { registerUser, loginUser, userProfile, updateProfile, allRestaurants, allMenus, viewOrders, placeOrder, getMenu, getRestaurant, searchRestaurants } = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/mon-compte',  authMiddleware ,userProfile);
+router.get('/mon-compte', authMiddleware, userProfile);
 router.patch('/edit-user', authMiddleware, updateProfile);
 router.get('/all-restaurants', authMiddleware, allRestaurants);
 router.get('/all-menus', authMiddleware, allMenus);
+router.get('/orders', authMiddleware, viewOrders);
+router.post('/place-order', authMiddleware, placeOrder);
+router.get('/menu/:id', authMiddleware, getMenu);
+router.get('/restaurant/:id', authMiddleware, getRestaurant);
+router.get('/search-restaurants', authMiddleware, searchRestaurants);
 
 
 module.exports = router;
